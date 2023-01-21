16fd3817c89620ecd7f75543c0932de9553ba80e
DurkaFlurk <113761843+DurkaFlurk@users.noreply.github.com>
2023-01-21 01:55:15 -0800

    Update and rename omni_gta7litewifi.mk to twrp_gta7litewifi.mk
    twrp

--- twrp_gta7litewifi.mk
+++ twrp_gta7litewifi.mk
@@ -14,12 +14,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/gta7litewifi/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta7litewifi
-PRODUCT_NAME := omni_gta7litewifi
+PRODUCT_NAME := twrp_gta7litewifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T220
 PRODUCT_MANUFACTURER := samsung
